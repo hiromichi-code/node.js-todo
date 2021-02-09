de55651811588bdbdd40b7ae23c6ed77023a46c6
Hiro <hiromichi.s.617@gmail.com>
2021-02-10 06:49:43 +0900

    データベース動かず

--- todoapp/routes/index.js
+++ todoapp/routes/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
 
 let todos = [];
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '197561789opinO`',
+  database: 'todo_app'
+});
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
     title: 'ToDo App',
     todos: todos,
   });
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   connection.connect((err) => {
     if (err) {
       console.log('error connecting: ' + err.stack);
@@ -20,16 +27,7 @@ router.post('/', function(req, res, next) {
     console.log('success');
   });
   const todo = req.body.add;
-  todos.push(todo);
   res.redirect('/');
 });
 
 module.exports = router;
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '197561789opinO`',
-  database: 'todo_app'
-});
